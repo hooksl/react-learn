6e3fbd7fcb806de089abebe0028e01412a0a2786
lightning <lightning004@outlook.com>
2024-05-28 00:17:12 +0800

    第一个组件

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Gallery from "./learn/01-describing-the-ui/your-first-component/your-first-component";
 
 function App() {
 
     return (
         <div>
             <h1>你好,react!</h1>
+            <Gallery />
         </div>
     )
 }

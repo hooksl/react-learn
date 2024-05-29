ff19d495cfb2f9409b0e33fc95995c093e117e72
lightning <lightning004@outlook.com>
2024-05-29 19:09:15 +0800

    在组件间共享状态

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
-import Form from './learn/03-managing-state/02-choosing-the-state-structure/choosing-the-state-structure'
+import Accordion from './learn/03-managing-state/03-sharing-state-between-components/sharing-state-between-components'
 function App() {
     return (
         <div>
-            <Form />
+            <Accordion />
         </div>
     )
 }
